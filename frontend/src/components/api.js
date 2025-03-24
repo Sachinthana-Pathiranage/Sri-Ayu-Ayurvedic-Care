00f381c50f10a92d4feca72b5a75f8db6efa7ab6
NabeehaThushan <183045503+NabeehaThushan@users.noreply.github.com>
2025-03-24 06:42:29 +0530

    Add files via upload

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -5,8 +5,9 @@ const API_URL = "http://127.0.0.1:5000"; // Update this if deployed
 export const fetchPredictions = async (formData) => {
   try {
     console.log("Sending request with:", formData);
-    const response = await axios.post(`${API_URL}/predict`, formData, {
+    const response = await axios.post(`${API_URL}/tourism/predict`, formData, {
       headers: {
+        "method": "POST",
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
       },
