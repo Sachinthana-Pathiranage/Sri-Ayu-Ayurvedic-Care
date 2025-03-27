50e29a8a9b5b4f021044fef2571c826cc94f32af
Kavindi Wijesundara <134049243+kavi-910@users.noreply.github.com>
2025-03-27 06:39:24 +0530

    feat(backend-final): modify backend final and landing page

--- backend/backend-final/backend-final/backend-final/app.py
+++ backend/backend-final/backend-final/backend-final/app.py
@@ -6,15 +6,15 @@ from routes.dosha_api import dosha_bp
 from routes.Disease_Prediction import disease_bp
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder=None)
+app = Flask(__name__, static_folder= "none")
 CORS(app)
 
-# Register Blueprints with URL prefixes
 app.register_blueprint(tourism_bp, url_prefix='/tourism')
 app.register_blueprint(dosha_bp, url_prefix='/dosha')
 app.register_blueprint(disease_bp, url_prefix='/disease')
 #app.register_blueprint(outcome_bp, url_prefix='/outcome')
 
+#-----------------------------------------------------------Dosha_Page-------------------------------------------------------------------------------------------------------
 @app.route("/dosha_frontend")
 def serve_dosha_index():
     dosha_build_dir = os.path.join(
@@ -37,29 +37,7 @@ def serve_dosha_static(path):
     )
     return send_from_directory(dosha_build_dir, path)
 
-
-@app.route("/tourism_frontend")
-def serve_tourism_index():
-    tourism_build_dir = os.path.join(
-        os.path.dirname(__file__),
-        "..",
-        "frontend-final",
-        "tourism_page",
-        "build"
-    )
-    return send_from_directory(tourism_build_dir, "index.html")
-
-@app.route("/tourism_frontend/<path:path>")
-def serve_tourism_static(path):
-    tourism_build_dir = os.path.join(
-        os.path.dirname(__file__),
-        "..",
-        "frontend-final",
-        "tourism_page",
-        "build"
-    )
-    return send_from_directory(tourism_build_dir, path)
-
+#------------------------------------------------------------Treatment_Recommendation_Page---------------------------------------------------------------------------------
 @app.route("/recommendation_frontend")
 def serve_disease_index():
     disease_build_dir = os.path.join(
@@ -71,6 +49,18 @@ def serve_disease_index():
     )
     return send_from_directory(disease_build_dir, "index.html")
 
+@app.route("/recommendation_frontend/<path:path>")
+def serve_disease_static(path):
+    disease_build_dir = os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "frontend-final",
+        "disease_page",
+        "build"
+    )
+    return send_from_directory(disease_build_dir, path)
+
+#-------------------------------------------------------Outcome_Prediction_Page---------------------------------------------------------------------------------------------
 @app.route("/outcome_frontend")
 def serve_outcome_index():
     outcome_build_dir = os.path.join(
@@ -93,23 +83,58 @@ def serve_outcome_static(path):
     )
     return send_from_directory(outcome_build_dir, path)
 
-
-@app.route("/recommendation_frontend/<path:path>")
-def serve_disease_static(path):
-    disease_build_dir = os.path.join(
+#----------------------------------------------------------Tourism_Page----------------------------------------------------------------------------------------------------
+@app.route("/tourism_frontend")
+def serve_tourism_index():
+    tourism_build_dir = os.path.join(
         os.path.dirname(__file__),
         "..",
         "frontend-final",
-        "disease_page",
+        "tourism_page",
         "build"
     )
-    return send_from_directory(disease_build_dir, path)
+    return send_from_directory(tourism_build_dir, "index.html")
 
+@app.route("/tourism_frontend/<path:path>")
+def serve_tourism_static(path):
+    tourism_build_dir = os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "frontend-final",
+        "tourism_page",
+        "build"
+    )
+    return send_from_directory(tourism_build_dir, path)
+
+#------------------------------------------------------------Signin_Page----------------------------------------------------------------------------------------------------
+@app.route("/signinpage")
+def serve_signin_index():
+    signin_build_dir = os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "frontend-final",
+        "signin_page",
+        "build"
+    )
+    return send_from_directory(signin_build_dir, "index.html")
+
+@app.route("/signinpage/<path:path>")
+def serve_signin_static(path):
+    signin_build_dir = os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "frontend-final",
+        "signin_page",
+        "build"
+    )
+    return send_from_directory(signin_build_dir, path)
+
+#------------------------------------------------------------Landing_Page---------------------------------------------------------------------------------------------------
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve_landing(path):
-    # Hardcoded absolute path to your build folder
-    landing_build_dir = r"C:\Users\prabh\Documents\GitHub\Sri-Ayu-Ayurvedic-Care-new3\backend\backend-final\backend-final\frontend-final\landing_page\build"
+
+    landing_build_dir = r"C:\Users\BM\IdeaProjects\DSGP - Sri Ayu\Sri-Ayu-Ayurvedic-Care\backend\backend-final\backend-final\frontend-final\landing_page\build"
     abs_build_dir = os.path.abspath(landing_build_dir)
     full_path = os.path.join(abs_build_dir, path)
 
